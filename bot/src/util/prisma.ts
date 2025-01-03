aa8a1dc652c51fc2fc3e7de4f0687ac896b27ff7
polypixeldev <me@sfernandez.dev>
2025-01-03 14:45:48 -0500

    remove global prisma

--- bot/src/util/prisma.ts
+++ bot/src/util/prisma.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = global.prisma || new PrismaClient();
-global.prisma = prisma;
+const prisma = new PrismaClient();
 
 export { prisma };
